6fd44e54e192a632e0c06840e7689cd1abb27a58
geowarin <geowarin@gmail.com>
2016-01-19 19:55:10 +0100

    ducks

--- app/reducers/counter.js
+++ app/reducers/counter.js
@@ -1,4 +1,17 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+export function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+export function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  };
+}
 
 export default function counter(state = 0, action) {
   switch (action.type) {
