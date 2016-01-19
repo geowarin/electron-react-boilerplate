e458398473ec93b16c16c608a93eec45eb152d38
geowarin <geowarin@gmail.com>
2016-01-19 21:11:22 +0100

    Photon

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.styl';
 
-
 export default class Home extends Component {
   render() {
-    return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
-      </div>
+    return (<div>Home</div>
     );
   }
 }
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import isDev from 'isdev';
-import DevTools from './DevTools'
+import DevTools from './DevTools';
+
+import {Window, Toolbar, Content, Pane, NavGroup, NavGroupItem, NavTitle} from 'react-photonkit';
 
 export default class App extends Component {
   static propTypes = {
@@ -10,9 +12,28 @@ export default class App extends Component {
   render() {
     const ReduxMonitor = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
     return (
-      <div> {this.props.children}
-	{ReduxMonitor}
-      </div>
+      <Window>
+	<Toolbar title="basic template"/>
+	<Content>
+	  <Pane ptSize="sm" sidebar>
+	    <NavGroup activeKey={1} onSelect={(key) => this.handleSelect(key)} draggable>
+	      <NavTitle>Menu</NavTitle>
+	      <NavGroupItem eventKey="home" glyph="home" text="home"/>
+	      <NavGroupItem eventKey="counter" glyph="clock" text="counter"/>
+	    </NavGroup>
+	  </Pane>
+
+	  <div className="padded-more">
+	    {this.props.children}
+	    {ReduxMonitor}
+	  </div>
+	</Content>
+	<Toolbar psType="footer"/>
+      </Window>
     );
+  }
+
+  handleSelect(key) {
+    console.log(key);
   }
 }
