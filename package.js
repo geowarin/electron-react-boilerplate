e510f0702b10de5ab3bc42d744ea15f3c2516545
geowarin <geowarin@gmail.com>
2016-01-19 14:43:13 +0100

    refactor packaging script

--- package.js
+++ package.js
@@ -1,101 +1,78 @@
 'use strict';
 
 const os = require('os');
-const webpack = require('webpack');
-const cfg = require('./webpack.config.production.js');
 const packager = require('electron-packager');
-const del = require('del');
-const exec = require('child_process').exec;
+const rimraf = require('rimraf').sync;
+const exec = require('child_process').execSync;
 const pkg = require('./package.json');
 const devDeps = Object.keys(pkg.devDependencies);
 
-const appName = pkg.productName;
-// https://github.com/atom/electron/blob/master/docs/tutorial/application-packaging.md
-const shouldUseAsar = false;
 const shouldBuildAll = false;
 
+const iconPath = 'app/app'; // without extension
 const DEFAULT_OPTS = {
   dir: './',
-  name: appName,
-  asar: shouldUseAsar,
+  name: pkg.productName,
+  // https://github.com/atom/electron/blob/master/docs/tutorial/application-packaging.md
+  asar: false,
   ignore: [
     '/test($|/)',
     '/tools($|/)',
     '/release($|/)'
-  ].concat(devDeps.map(name => `/node_modules/${name}($|/)`))
+  ].concat(devDeps.map(name => `/node_modules/${name}($|/)`)),
+  version: getElectronVersion()
 };
 
-DEFAULT_OPTS.icon = 'app/app';
+const targets = !shouldBuildAll ? getAllTargets() : [{platform: os.platform(), arch: os.arch()}];
 
-// use the same version as the currently-installed electron-prebuilt
-exec('npm list electron-prebuilt', (err, stdout) => {
-  if (err) {
-    DEFAULT_OPTS.version = '0.36.2';
-  } else {
-    DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
+rimraf('release');
+targets.forEach((target) => packageApp(target));
+
+// utils
+function packageApp(target) {
+  console.log(`building: ${target.platform}-${target.arch}`);
+  const opts = Object.assign({}, DEFAULT_OPTS, target, {
+    icon: getIcon(target.platform),
+    prune: true,
+    out: `release/${target.platform}-${target.arch}`
+  });
+  packager(opts, log(target.platform, target.arch));
+}
+
+function getElectronVersion() {
+  try {
+    const out = exec('npm list electron-prebuilt').toString();
+    return out.split('electron-prebuilt@')[1].replace(/\s/g, '');
+  } catch (err) {
+    return '0.36.2';
   }
+}
 
-  startPack();
-});
+function getAllTargets() {
+  const allVersions = [];
+  const archs = ['ia32', 'x64'];
+  const platforms = ['linux', 'win32', 'darwin'];
 
-
-function startPack() {
-  console.log('start pack...');
-  webpack(cfg, (err, stats) => {
-    if (err) return console.error(err);
-    del('release')
-    .then(paths => {
-      if (shouldBuildAll) {
-        // build for all platforms
-        const archs = ['ia32', 'x64'];
-        const platforms = ['linux', 'win32', 'darwin'];
-
-        platforms.forEach(plat => {
-          archs.forEach(arch => {
-            pack(plat, arch, log(plat, arch));
-          });
-        });
-      } else {
-        // build for current platform only
-        pack(os.platform(), os.arch(), log(os.platform(), os.arch()));
+  platforms.forEach(platform => {
+    archs.forEach(arch => {
+      if (!(platform === 'darwin' && arch === 'ia32')) { // darwin 32 does not exist
+	allVersions.push({platform, arch})
       }
-    })
-    .catch(err => {
-      console.error(err);
     });
   });
+  return allVersions;
 }
 
-function pack(plat, arch, cb) {
-  // there is no darwin ia32 electron
-  if (plat === 'darwin' && arch === 'ia32') return;
-
-  const iconObj = {
-    icon: DEFAULT_OPTS.icon + (() => {
-      let extension = '.png';
-      if (plat === 'darwin') {
-        extension = '.icns';
-      } else if (plat === 'win32') {
-        extension = '.ico';
-      }
-      return extension;
-    })()
-  };
-
-  const opts = Object.assign({}, DEFAULT_OPTS, iconObj, {
-    platform: plat,
-    arch,
-    prune: true,
-    out: `release/${plat}-${arch}`
-  });
-
-  packager(opts, cb);
+function getIcon(platform) {
+  const iconExt = platform === 'darwin' ? '.icns' : platform === 'win32' ? '.ico' : '.png';
+  return iconPath + iconExt;
 }
 
-
-function log(plat, arch) {
-  return (err, filepath) => {
-    if (err) return console.error(err);
-    console.log(`${plat}-${arch} finished!`);
+function log(platform, arch) {
+  return (err) => {
+    if (err) {
+      return console.error(`Error building ${platform}-${arch}:`, err);
+    }
+    console.log(`${platform}-${arch} finished!`);
   };
 }
