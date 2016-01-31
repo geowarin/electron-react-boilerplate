d77201b21252f43d23096950195103f9411126b3
geowarin <geowarin@gmail.com>
2016-01-31 23:02:07 +0100

    tests: Add counter tests

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import styles from './Home.module.styl';
 
 export default class Home extends Component {
---
+++ test/components/Counter.spec.js
@@ -0,0 +1,33 @@
+import expect, { createSpy } from 'expect'
+import {shallow} from 'enzyme';
+
+import React from 'react';
+import Counter from '../../app/components/Counter';
+
+const increment = createSpy();
+const decrement = createSpy();
+const counter = 42;
+let props = {increment, decrement, counter};
+
+describe('components', () => {
+
+  describe('MyComponent', () => {
+
+    it('should render the counter', () => {
+      const component = shallow(<Counter {...props} />);
+      expect(component.find('.counter').text()).toEqual('42');
+    });
+
+    it('should call increment on click', () => {
+      const component = shallow(<Counter {...props} />);
+      component.find('button').first().simulate('click');
+      expect(increment).toHaveBeenCalled();
+    });
+
+    it('should call decrement on click', () => {
+      const component = shallow(<Counter {...props} />);
+      component.find('button').at(1).simulate('click');
+      expect(decrement).toHaveBeenCalled();
+    });
+  });
+});
