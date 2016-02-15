08857347dca8b60c10e8cf10957075218da18ceb
geowarin <geowarin@gmail.com>
2016-02-15 14:51:10 +0100

    Remove base config

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -5,48 +5,59 @@ const webpack = require('webpack');
 const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 const baseConfig = require('./webpack.config.base');
 
-const config = Object.create(baseConfig);
+const path = require('path');
 
-config.debug = true;
-
-config.devtool = 'cheap-module-eval-source-map';
-
-config.entry = [
-  'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
-  './app/index'
-];
-
-config.output.publicPath = 'http://localhost:3000/dist/';
-
-config.module.loaders.push({
-  test: /^((?!\.module).)*\.styl$/,
-  loaders: [
-    'style-loader',
-    'css-loader',
-    'stylus-loader'
+const config = {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+    './app/index'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /^((?!\.module).)*\.styl$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        'stylus-loader'
+      ]
+    }, {
+      test: /\.module\.styl/,
+      loaders: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!',
+        'stylus-loader'
+      ]
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:3000/dist/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ],
+  externals: [
+    // put your node 3rd party libraries which can't be built with webpack here (mysql, mongodb, and so on..)
   ]
-}, {
-  test: /\.module\.styl/,
-  loaders: [
-    'style-loader',
-    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!',
-    'stylus-loader'
-  ]
-});
+};
 
-
-config.plugins.push(
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('development')
-    }
-  })
-);
-
-// when 2.0 is stable, might be worth checking the electron-renderer target
-// https://github.com/webpack/webpack/blob/master/lib/WebpackOptionsApply.js#L150
 config.target = webpackTargetElectronRenderer(config);
 
 module.exports = config;
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -4,44 +4,56 @@
 const webpack = require('webpack');
 const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const baseConfig = require('./webpack.config.base');
+const path = require('path');
 
-
-const config = Object.create(baseConfig);
-
-config.devtool = 'source-map';
-
-config.entry = './app/index';
-
-config.output.publicPath = '../dist/';
-
-config.module.loaders.push({
-  test: /^((?!\.module).)*\.styl/,
-  loader: ExtractTextPlugin.extract(
-    'css-loader!stylus-loader'
-  )
-}, {
-  test: /\.module\.styl$/,
-  loader: ExtractTextPlugin.extract(
-    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus-loader'
-  )
-});
-
-config.plugins.push(
-  new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      screw_ie8: true,
-      warnings: false
-    }
-  }),
-  new ExtractTextPlugin('style.css', {allChunks: true})
-);
+const config = {
+  devtool: 'source-map',
+  entry: './app/index',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /^((?!\.module).)*\.styl/,
+      loader: ExtractTextPlugin.extract(
+        'css-loader!stylus-loader'
+      )
+    }, {
+      test: /\.module\.styl$/,
+      loader: ExtractTextPlugin.extract(
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus-loader'
+      )
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '../dist/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+    new ExtractTextPlugin('style.css', {allChunks: true})
+  ],
+  externals: [
+    // put your node 3rd party libraries which can't be built with webpack here (mysql, mongodb, and so on..)
+  ]
+};
 
 config.target = webpackTargetElectronRenderer(config);
 
